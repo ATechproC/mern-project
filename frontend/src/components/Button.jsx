e74265bbdf9bdaa5514fc963b23f624b29a2a053
ATechproC <choraichianass@gmail.com>
2025-11-20 18:39:46 +0100

    add Admin pages

--- frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({ speciality }) => {
-    return <div className='whitespace-nowrap px-3 py-1 mx-auto hover:bg-secondary-color w-[150px] border-[1px] rounded-[3px] border-gray-500 cursor-pointer transition duration-3000'>
-        {speciality}
-    </div>
-}
-
-export default Button
